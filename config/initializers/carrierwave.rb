240f3d60098bf11b06be38c0552ca53fb04a04c8
Jason <jason.wang@sun-innovation.com>
2015-07-07 16:32:49 +0800

    save attachment to amazon s3 by fog gem

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,15 @@
+CarrierWave.configure do |config|
+  config.root = Rails.root.join('tmp')
+  config.cache_dir = 'carrierwave'
+
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: AMAZON[Rails.env]['access_key_id'],
+    aws_secret_access_key: AMAZON[Rails.env]['secret_access_key'],
+    region: AMAZON[Rails.env]['region']
+  }
+
+  config.fog_directory  = AMAZON[Rails.env]['bucket']
+  config.fog_public     = true
+  config.fog_attributes = { 'Cache-Control' => 'max-age=315576000' }
+end
