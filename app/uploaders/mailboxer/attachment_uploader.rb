291551af7049c3f6780ecc3705fe31e8fe36e8d9
Jason <jason.wang@sun-innovation.com>
2015-07-08 17:31:45 +0800

    add some file restrick to model

--- app/uploaders/mailboxer/attachment_uploader.rb
+++ app/uploaders/mailboxer/attachment_uploader.rb
@@ -1,3 +1,11 @@
 class Mailboxer::AttachmentUploader < CarrierWave::Uploader::Base
   storage :fog
+
+  def store_dir
+    "message/#{model.id}/attachment"
+  end
+
+  def extension_white_list
+    %w(jpg jpeg png pdf)
+  end
 end
